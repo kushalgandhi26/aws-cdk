76707fe4234835cd3b0cd80559eacfbf9eb23758
kushalgandhi26 <kushalgandhi2601@gmail.com>
2024-05-14 19:39:08 +0530

    version 1.0.0

--- lib/ServerlessStack.ts
+++ lib/ServerlessStack.ts
@@ -1,7 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
